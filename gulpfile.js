9efde340905c2d463d41e614a1804309f6982963
Andy Dayton <amdayton@gmail.com>
2015-11-01 12:16:37 -0500

    Updated dev workflow

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,30 @@ var browserify = require('browserify');
 var less = require('gulp-less');
 var source = require('vinyl-source-stream');
 
-
+var paths = {
+  js: ['web/js/src/**/*.js'],
+  less: ['web/css/src/**/*.less']
+};
 
 // css preprocessor
 gulp.task('css', function() {
-	return gulp.src('./web/css/**/*.less')
+	return gulp.src('./web/css/src/**/*.less')
 		.pipe(less({}))
 		.pipe(gulp.dest('./web/css/'));
 });
 
 // javascript preprocessor
 gulp.task('js', function() {
-	return browserify('./web/js/main-src.js')
+	return browserify('./web/js/src/main.js')
 		.bundle()
 		.pipe(source('main.js'))
 		.pipe(gulp.dest('./web/js/'));
 });
+
+
+gulp.task('watch', function() {
+	gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.less, ['css']);
+})
+
+gulp.task('default', ['watch', 'js', 'css'])
--- web/js/src/main.js
+++ web/js/src/main.js
@@ -1,5 +1,5 @@
-_ = require('lodash');
 osc = require('osc/dist/osc-browser');
 
 // seems to be working!
-console.log('OSC! ', osc);
+console.log('OSC!!!! ', osc);
+
